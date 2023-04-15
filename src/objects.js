4c72f79d42c22227315e7047720d52470f549db1
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-15 21:13:04 +0800

    added another project

--- src/objects.js
+++ src/objects.js
@@ -1,5 +1,11 @@
 export const frontend = [
   {
+    imgURL: 'https://drive.google.com/uc?export=view&id=1LREASqG9B4Xz8vbQojLu-ZQ3wEBI9_Bk',
+    link: 'https://millenmark.github.io/mini-project-result-summary-component/',
+    name: 'Results summary component',
+    tags: ['html', 'css', 'javascript']
+  },
+  {
     imgURL: 'https://drive.google.com/uc?export=view&id=1dQEXs72uIiiJqqQTpBL0B09M7MPpLlZL',
     link: 'https://millenmark.github.io/mini-project-qr-code-component/',
     name: 'QR code component',
