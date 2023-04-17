60b468d079b7816cf78c3f8b2dd1ae61f2495910
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-17 15:40:50 +0800

    Update frontProjects.js

--- src/frontProjects.js
+++ src/frontProjects.js
@@ -1,7 +1,7 @@
 export const frontend = [
   {
     "id":"1",
-    "name":"result summary component",
+    "name":"results summary component",
     "img_link": "https://tiftyleb.sirv.com/frontmentor/result_summary.png",
     "tags": "html,css,javascript",
     "live_link": "https://millenmark.github.io/mini-project-result-summary-component/"
