52feb2d16bfec3d44acee97f1eebb7f644f7372e
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-17 15:28:00 +0800

    remove

--- src/frontProjects.js
+++ src/frontProjects.js
@@ -1,13 +1,6 @@
 export const frontend = [
   {
     "id":"1",
-    "name":"QR code component",
-    "img_link": "https://tiftyleb.sirv.com/frontmentor/dark.png",
-    "tags": "html,css",
-    "live_link": "https://millenmark.github.io/mini-project-qr-code-component/"
-  },
-  {
-    "id":"2",
     "name":"result summary component",
     "img_link": "https://tiftyleb.sirv.com/frontmentor/result_summary.png",
     "tags": "html,css,javascript",
