4eb9bc8041678e242edc8ec14cb1b5855c7fb4ff
Millenmark <millenmark.iv@gmail.com>
2023-04-23 22:23:41 +0800

    Update frontProjects.js

--- src/frontProjects.js
+++ src/frontProjects.js
@@ -1,7 +1,7 @@
 export const frontend = [
   {
     "id":"1",
-    "name":"results summary component",
+    "name":"results summary",
     "img_link": "https://tiftyleb.sirv.com/frontendsolutions/results_summary.jpg",
     "tags": "html,css,javascript",
     "live_link": "https://millenmark.github.io/mini-project-result-summary-component/"
