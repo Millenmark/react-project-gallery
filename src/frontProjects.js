a190959b4e0a8f40fd429fd98d3f37c9852c57da
Millenmark <millenmark.iv@gmail.com>
2023-05-03 14:23:40 +0800

    Update frontProjects.js

--- src/frontProjects.js
+++ src/frontProjects.js
@@ -5,5 +5,12 @@ export const frontend = [
     "img_link": "https://tiftyleb.sirv.com/frontendsolutions/results_summary.jpg",
     "tags": "html,css,javascript",
     "live_link": "https://millenmark.github.io/mini-project-result-summary-component/"
+  },
+  {
+    "id": "2",
+    "name": "age calculator app",
+    "img_link": "https://tiftyleb.sirv.com/frontendsolutions/age_calculator_app.jpg",
+    "tags": "html, css, javascript",
+    "live_link": "https://millenmark.github.io/mini-project-age-calculator-app/"
   }
 ]
