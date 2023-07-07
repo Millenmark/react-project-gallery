fca5907a0de66e7fad9d7984377a729c3c436c50
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 17:11:15 +0800

    Create mentor.js

---
+++ src/services/mentor.js
@@ -0,0 +1,23 @@
+export const mentor = [
+  {
+    name: "results summary",
+    img_link: "https://tiftyleb.sirv.com/frontendsolutions/results_summary.jpg",
+    tags: "html,css,javascript",
+    live_link:
+      "https://millenmark.github.io/mini-project-result-summary-component/",
+  },
+  {
+    name: "age calculator app",
+    img_link:
+      "https://tiftyleb.sirv.com/frontendsolutions/age_calculator_app.jpg",
+    tags: "html,css,javascript",
+    live_link: "https://millenmark.github.io/mini-project-age-calculator-app/",
+  },
+  {
+    name: "pricing component",
+    img_link:
+      "https://tiftyleb.sirv.com/frontendsolutions/pricing_component.jpg",
+    tags: "react,tailwind",
+    live_link: "https://millen-react-pricing-component.vercel.app/",
+  },
+];
