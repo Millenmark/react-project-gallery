901f2b3862937c8d1c3b536a459ff7b39b941ed2
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 17:11:18 +0800

    Create personal.js

---
+++ src/services/personal.js
@@ -0,0 +1,14 @@
+export const personal = [
+  {
+    name: "crystal tec website",
+    img_link: "https://tiftyleb.sirv.com/personalprojects/ctech_website.jpg",
+    tags: "react,css",
+    live_link: "https://crystaltec-sample.netlify.app/",
+  },
+  {
+    name: "crystal tec inventory",
+    img_link: "https://tiftyleb.sirv.com/personalprojects/ctech_inventory.jpg",
+    tags: "laravel,mysql",
+    live_link: "https://crystaltecinventory.sourceupdigital.com/login",
+  },
+];
