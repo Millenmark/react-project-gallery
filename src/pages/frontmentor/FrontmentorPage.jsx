453cf6607b8f3e42256f0c462c976bdc3ee4e982
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-07 17:11:06 +0800

    Update FrontmentorPage.jsx

--- src/pages/frontmentor/FrontmentorPage.jsx
+++ src/pages/frontmentor/FrontmentorPage.jsx
@@ -1,29 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import style from "./FrontmentorPage.module.css";
 import { Container, ProjectWrapper } from "../../ui";
 import { Card, Loader } from "../../components";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { mentor } from "../../services/mentor";
 
 const FrontmentorPage = () => {
-  const fetchProjects = async () => {
-    const response = await axios.get(
-      `${import.meta.env.VITE_BACKEND}/projects/frontend`
-    );
-    return response.data;
-  };
+  const [isLoading, setIsLoading] = useState(false);
+  // const fetchProjects = async () => {
+  //   const response = await axios.get(
+  //     `${import.meta.env.VITE_BACKEND}/projects/frontend`
+  //   );
+  //   return response.data;
+  // };
 
-  const { data, isLoading, isError } = useQuery(
-    ["frontendProjects"],
-    fetchProjects
-  );
+  // const { data, isLoading, isError } = useQuery(
+  //   ["frontendProjects"],
+  //   fetchProjects
+  // );
 
-  if (isError) {
-    return (
-      <div style={{ textAlign: "center" }}>
-        Error occurred while fetching projects.
-      </div>
-    );
+  // if (isError) {
+  //   return (
+  //     <div style={{ textAlign: "center" }}>
+  //       Error occurred while fetching projects.
+  //     </div>
+  //   );
+  // }
+
+  if (mentor.length === 0) {
+    setIsLoading(true);
   }
 
   return (
@@ -32,15 +38,17 @@ const FrontmentorPage = () => {
         {isLoading ? (
           <Loader />
         ) : (
-          data.map((item, index) => (
-            <Card
-              key={index}
-              imgURL={item.img_link}
-              name={item.name}
-              tags={item.tags}
-              link={item.live_link}
-            />
-          ))
+          mentor
+            .map((item, index) => (
+              <Card
+                key={index}
+                imgURL={item.img_link}
+                name={item.name}
+                tags={item.tags}
+                link={item.live_link}
+              />
+            ))
+            .reverse()
         )}
       </ProjectWrapper>
     </Container>
